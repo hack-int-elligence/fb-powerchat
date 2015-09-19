d5e4f9692d28c5b1c72d03e6ee1671e25b0b731d
kbharathala <kbharathala@gmail.com>
2015-09-19 01:07:34 -0400

    Finished youtube

--- server.py
+++ server.py
@@ -25,11 +25,26 @@ def giphy():
 		url = 'http://api.giphy.com/v1/gifs/search?q='+ mod_input + '&api_key=dc6zaTOxFJmzC'
 		data = urllib.urlopen(url).read()
 		json_list = json.loads(data)['data']
-		print(json_list)
 		if len(json_list) == 0:
 			return "{'type': 'giphy', 'data':'no data', 'result':'failure'}"
 		else:
 			return "{'type': 'giphy', 'data':" + json_list[0]['embed_url'] + ", 'result':'success'}"
+
+@app.route('/youtube', methods=['GET'])
+def youtube():
+	if request.method == 'GET':
+		mod_input = request.args.get('keyword').replace(" ", "+")
+		API_key = 'AIzaSyBQB5ELmm4MbpQUJLT3xR9rfyhYFEksgvc'
+		url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + mod_input + "&key=" + API_key
+		data = urllib.urlopen(url).read()
+		json_list = json.loads(data)['items']
+		if len(json_list) == 0:
+			return "{'type': 'youtube', 'data':'no data', 'result':'failure'}"
+		else:
+			video_id = json_list[0]['id']['videoId']
+			url = 'https://www.youtube.com/watch?v=' + video_id
+			return "{'type': 'youtube', 'data':" + url + ", 'result':'success'}"
+
 
 if __name__ == "__main__":
     app.run()
