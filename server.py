d252a20cd6f83b0367fe4a2df9fe282926a866e0
kbharathala <kbharathala@gmail.com>
2015-09-19 00:11:48 -0400

    adding server and giphy

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request
+from flask_sockets import Sockets
+
+import giphy
+import urllib
+import json
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
+
+@app.route('/')
+def hello():
+	return 'Hello World!'
+
+@app.route('/giphy', methods=['GET'])
+def giphy():
+	if request.method == 'GET':
+		mod_input = request.args.get('keyword').replace(" ", "+")
+		url = 'http://api.giphy.com/v1/gifs/search?q='+ mod_input + '&api_key=dc6zaTOxFJmzC'
+		data = urllib.urlopen(url).read()
+		json_list = json.loads(data)['data']
+		print(json_list)
+		if len(json_list) == 0:
+			return "{'type': 'giphy', 'data':'No data', 'result':'Failure'}"
+		else:
+			return "{'type': 'giphy', 'data':json_list[0]['embed_url'], 'result':'Success'}"
+
+if __name__ == "__main__":
+    app.run()
