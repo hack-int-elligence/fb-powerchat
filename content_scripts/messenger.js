0c45212b7769009ef9f33a5197dc898a7ae0066f
kbharathala <kbharathala@gmail.com>
2015-09-19 17:50:57 -0400

    added map functionality

--- content_scripts/messenger.js
+++ content_scripts/messenger.js
@@ -109,6 +109,40 @@ var checkMessages = function() {
 	});
 };
 
+var init_map = function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+};
+
+var handleLocationError = function(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+};
+
 var init_microphone = function() {
 	$('._4rv4').prepend('<li title="Input messages via voice"><img style="width: 30px; height: 30px;" id="microphone" src="' + quiet_mic + '" "></li>');
 	var final_transcript = '';
